e22f81573ba8d72de024be4345272c4edab1d0ff
Mir Ashikur Rahman <ashik82@gmail.com>
2021-10-05 23:58:42 +0600

    Update Debounce_example.c

--- Debounce_example.c
+++ Debounce_example.c
@@ -1,13 +1,13 @@
 static void init_pushButton() // pc1 push button > turns on pd3 led
  {	  
-          PORTC.DIR &= ~(PIN1_bm);	  PORTC.PIN1CTRL |= PORT_PULLUPEN_bm; // pc7 as push button to be debounced with timer B1 one shot 10ms delay
+          PORTC.DIR &= ~(PIN1_bm);	  PORTC.PIN1CTRL |= PORT_PULLUPEN_bm; // pc1 as push button to be debounced with timer B1 as one shot 10ms delay
 	  
 	  PORTD.DIR |= PIN3_bm; PORTD.OUT |= PIN3_bm; // pd3 as Led output
 	  
 	 EVSYS.CHANNEL3 = EVSYS_CHANNEL3_PORTC_PIN1_gc;
 	 EVSYS.USERTCB1CAPT = EVSYS_USER_CHANNEL3_gc;
 
-// TCB_1_init(void) ...keeping tcb0 free , since i am using it for millis
+// TCB_1 init ...keeping tcb0 free , since i am using it for millis
 	 TCB1.CCMP = 0xFFFF;
 	 TCB1.CTRLB = TCB_CNTMODE_SINGLE_gc;
 	 TCB1.EVCTRL = TCB_CAPTEI_bm | TCB_EDGE_bm;
